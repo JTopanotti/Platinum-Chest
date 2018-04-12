cbf6094d8d2ac5c239681427b17c0a77b76cd0c3
JTopanotti <jontopanotti@gmail.com>
2018-04-11 23:40:11 -0300

    Atualizar layout Cadastro de Produtos

--- src/telas/CadastroProdutos.java
+++ src/telas/CadastroProdutos.java
@@ -52,126 +52,158 @@ public class CadastroProdutos extends JInternalFrame {
 		setMaximizable(true);
 		setClosable(true);
 		setRootPaneCheckingEnabled(false);
-		setBounds(100, 100, 551, 343);
+		setBounds(100, 100, 551, 428);
 		getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 398, 294);
+		panel.setBounds(13, 7, 398, 356);
 		getContentPane().add(panel);
 		panel.setLayout(null);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(106, 36, 265, 20);
-		panel.add(textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel lblNome = new JLabel("Nome:");
-		lblNome.setBounds(65, 39, 31, 14);
-		panel.add(lblNome);
 		
 		JLabel label = new JLabel("");
 		label.setBounds(50, 39, 46, 14);
 		panel.add(label);
 		
-		JLabel lblSetor = new JLabel("Setor:");
-		lblSetor.setBounds(66, 64, 30, 14);
-		panel.add(lblSetor);
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(69, 18, 317, 29);
+		panel.add(panel_2);
+		panel_2.setLayout(new BorderLayout(0, 0));
 		
 		JLabel lblItem = new JLabel("Item:");
-		lblItem.setBounds(70, 14, 26, 14);
-		panel.add(lblItem);
+		panel_2.add(lblItem, BorderLayout.WEST);
 		
 		textField = new JTextField();
+		panel_2.add(textField, BorderLayout.CENTER);
 		textField.setEditable(false);
-		textField.setBounds(106, 11, 86, 20);
-		panel.add(textField);
 		textField.setColumns(10);
 		
+		JButton btnPesquisar = new JButton("Pesquisar");
+		panel_2.add(btnPesquisar, BorderLayout.EAST);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(65, 77, 190, 29);
+		panel.add(panel_3);
+		panel_3.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblSetor = new JLabel("Setor:");
+		panel_3.add(lblSetor, BorderLayout.WEST);
+		
 		JComboBox list = new JComboBox();
+		panel_3.add(list, BorderLayout.CENTER);
 		list.setSelectedIndex(0);
-		list.setBounds(106, 61, 117, 20);
-		panel.add(list);
 		
-		JLabel lblNF = new JLabel("NF:");
-		lblNF.setBounds(79, 114, 17, 14);
-		panel.add(lblNF);
+		JPanel panel_4 = new JPanel();
+		panel_4.setBounds(69, 106, 141, 29);
+		panel.add(panel_4);
+		panel_4.setLayout(new BorderLayout(0, 0));
 		
-		formattedTextField = new JFormattedTextField();
-		formattedTextField.setBounds(106, 111, 117, 20);
-		panel.add(formattedTextField);
+		JLabel lblValor = new JLabel("Valor:");
+		panel_4.add(lblValor, BorderLayout.WEST);
+		
+		textField_2 = new JTextField();
+		panel_4.add(textField_2, BorderLayout.CENTER);
+		textField_2.setColumns(10);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBounds(21, 166, 198, 29);
+		panel.add(panel_5);
+		panel_5.setLayout(new BorderLayout(0, 0));
 		
 		JLabel lblDataCompra = new JLabel("Data Compra:");
-		lblDataCompra.setBounds(29, 139, 67, 14);
-		panel.add(lblDataCompra);
+		panel_5.add(lblDataCompra, BorderLayout.WEST);
+		
+		JFormattedTextField formattedTextField_1 = new JFormattedTextField();
+		panel_5.add(formattedTextField_1, BorderLayout.CENTER);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setBounds(61, 48, 225, 29);
+		panel.add(panel_6);
+		panel_6.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNome = new JLabel("Nome:");
+		panel_6.add(lblNome, BorderLayout.WEST);
+		
+		textField_1 = new JTextField();
+		panel_6.add(textField_1, BorderLayout.CENTER);
+		textField_1.setColumns(10);
+		
+		JPanel panel_7 = new JPanel();
+		panel_7.setBounds(85, 135, 177, 29);
+		panel.add(panel_7);
+		panel_7.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNF = new JLabel("NF:");
+		panel_7.add(lblNF, BorderLayout.WEST);
+		
+		formattedTextField = new JFormattedTextField();
+		panel_7.add(formattedTextField, BorderLayout.CENTER);
+		
+		JPanel panel_8 = new JPanel();
+		panel_8.setBounds(31, 195, 260, 29);
+		panel.add(panel_8);
+		panel_8.setLayout(new BorderLayout(0, 0));
 		
 		JLabel lblFornc = new JLabel("Fornecedor:");
-		lblFornc.setBounds(37, 164, 59, 14);
-		panel.add(lblFornc);
+		panel_8.add(lblFornc, BorderLayout.WEST);
 		
 		textField_3 = new JComboBox();
-		textField_3.setBounds(106, 161, 139, 20);
-		panel.add(textField_3);
+		panel_8.add(textField_3, BorderLayout.CENTER);
+		
+		JPanel panel_9 = new JPanel();
+		panel_9.setBounds(28, 224, 236, 29);
+		panel.add(panel_9);
+		panel_9.setLayout(new BorderLayout(0, 0));
 		
 		JLabel lblDeprec = new JLabel("Deprecia\u00E7\u00E3o:");
-		lblDeprec.setBounds(33, 189, 63, 14);
-		panel.add(lblDeprec);
+		panel_9.add(lblDeprec, BorderLayout.WEST);
 		
 		textField_4 = new JTextField();
-		textField_4.setBounds(106, 186, 86, 20);
-		panel.add(textField_4);
+		panel_9.add(textField_4, BorderLayout.CENTER);
+		
+		JPanel panel_10 = new JPanel();
+		panel_10.setBounds(4, 312, 263, 29);
+		panel.add(panel_10);
+		panel_10.setLayout(new BorderLayout(0, 0));
+		
+		lblUsuarioG = new JLabel("Usu\u00E1rio Gera\u00E7\u00E3o:");
+		panel_10.add(lblUsuarioG, BorderLayout.WEST);
+		
+		textField_6 = new JTextField();
+		panel_10.add(textField_6, BorderLayout.CENTER);
+		textField_6.setEditable(false);
+		textField_6.setColumns(10);
+		
+		JPanel panel_11 = new JPanel();
+		panel_11.setBounds(22, 253, 200, 29);
+		panel.add(panel_11);
+		panel_11.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblDataG = new JLabel("Data Gera\u00E7\u00E3o:");
+		panel_11.add(lblDataG, BorderLayout.WEST);
+		
+		textField_7 = new JTextField();
+		panel_11.add(textField_7, BorderLayout.CENTER);
+		textField_7.setEditable(false);
+		textField_7.setColumns(10);
+		
+		JPanel panel_12 = new JPanel();
+		panel_12.setBounds(50, 282, 167, 29);
+		panel.add(panel_12);
+		panel_12.setLayout(new BorderLayout(0, 0));
 		
 		JLabel lblSituao = new JLabel("Situa\u00E7\u00E3o:");
-		lblSituao.setBounds(50, 265, 45, 14);
-		panel.add(lblSituao);
+		panel_12.add(lblSituao, BorderLayout.WEST);
 		
 		textField_5 = new JComboBox();
+		panel_12.add(textField_5, BorderLayout.CENTER);
 		textField_5.setModel(new DefaultComboBoxModel(new String[] {"1- Ativo", "2- Inativo"}));
-		textField_5.setBounds(106, 262, 86, 20);
-		panel.add(textField_5);
-		
-		JButton btnPesquisar = new JButton("Pesquisar");
 		btnPesquisar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		btnPesquisar.setBounds(195, 11, 79, 20);
-		panel.add(btnPesquisar);
-		
-		lblUsuarioG = new JLabel("Usu\u00E1rio Gera\u00E7\u00E3o:");
-		lblUsuarioG.setBounds(13, 214, 83, 14);
-		panel.add(lblUsuarioG);
-		
-		textField_6 = new JTextField();
-		textField_6.setEditable(false);
-		textField_6.setBounds(106, 211, 139, 20);
-		panel.add(textField_6);
-		textField_6.setColumns(10);
-		
-		JLabel lblDataG = new JLabel("Data Gera\u00E7\u00E3o:");
-		lblDataG.setBounds(26, 239, 70, 14);
-		panel.add(lblDataG);
-		
-		textField_7 = new JTextField();
-		textField_7.setEditable(false);
-		textField_7.setBounds(106, 237, 139, 20);
-		panel.add(textField_7);
-		textField_7.setColumns(10);
-		
-		JFormattedTextField formattedTextField_1 = new JFormattedTextField();
-		formattedTextField_1.setBounds(106, 136, 117, 20);
-		panel.add(formattedTextField_1);
-		
-		textField_2 = new JTextField();
-		textField_2.setBounds(106, 86, 86, 20);
-		panel.add(textField_2);
-		textField_2.setColumns(10);
-		
-		JLabel lblValor = new JLabel("Valor:");
-		lblValor.setBounds(68, 89, 28, 14);
-		panel.add(lblValor);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(418, 24, 116, 281);
+		panel_1.setBounds(418, 6, 116, 299);
 		getContentPane().add(panel_1);
 		panel_1.setLayout(null);
 		
